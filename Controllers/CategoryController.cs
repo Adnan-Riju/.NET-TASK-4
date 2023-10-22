2eb58f26ea2cf18240e54843707607ced28c3d56
unknown <adnanriju25@gmail.com>
2023-10-22 22:06:02 +0600

    new

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using Shop.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Category_Index()
+        {
+            var db = new ProjectEntities();
+            var data = db.tbl_Category.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult Create_Category()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create_Category(tbl_Category c )
+        {
+            var db = new ProjectEntities();
+            db.tbl_Category.Add(c);
+            db.SaveChanges();
+
+            return RedirectToAction("Category_Index");
+        }
+        [HttpGet]
+        public ActionResult Edit_Category(int CatId)
+        {
+            var db = new ProjectEntities();
+            var data = db.tbl_Category.Find(CatId);
+
+            return View(data);
+        }
+        public ActionResult Category_Details(int id)
+        {
+            var db = new ProjectEntities();
+            var dept = db.tbl_Category.Find(id);
+
+            return View(dept);
+
+        }
+        [HttpPost]
+        public ActionResult Edit_Category(tbl_Category d)
+        {
+            var db = new ProjectEntities();
+            var ex = db.tbl_Category.Find(d.Category);
+            ex.Category = d.Category;
+            db.SaveChanges();
+            return RedirectToAction("Category_Index,Category");
+        }
+        public ActionResult Delete(int CatId)
+        {
+
+            var db = new ProjectEntities();
+            var exdata = db.tbl_Category.Find(CatId);
+            db.tbl_Category.Remove(exdata);
+            db.SaveChanges();
+            return RedirectToAction("Category_Index,Category");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Shop.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ProjectEntities();
+            var data = db.tbl_Product.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tbl_Product d)
+        {
+            var db = new ProjectEntities();
+            db.tbl_Product.Add(d);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new ProjectEntities();
+            var data = db.tbl_Product.Find(id);
+            
+            return View(data);
+        }
+        public ActionResult Details(int id)
+        {
+            var db = new ProjectEntities();
+            var dept = db.tbl_Product.Find(id);
+         
+            return View(dept);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(tbl_Product d)
+        {
+            var db = new ProjectEntities();
+            var ex = db.tbl_Product.Find(d.Id);
+            ex.P_Name = d.P_Name;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var db = new ProjectEntities();
+            var ex = db.tbl_Product.Find(id);
+            db.tbl_Product.Remove(ex);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+      
+
+       
+        
+    }
+}
+    
